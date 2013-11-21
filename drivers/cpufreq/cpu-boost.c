c20acded30097bddda9cada077e3fcbe8937db1c
Rohit Gupta <rohgup@codeaurora.org>
2013-11-21 14:51:07 -0800

    cpufreq: Add a sync limit to cpu-boost
    Perform frequency synchronization only when source CPU's frequency
    is less than sync_threshold, else sync to the sync_threshold.
    
    Change-Id: I544c414568d4e015b80ce5891dd215275bac95da
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -40,6 +40,8 @@ static struct workqueue_struct *boost_rem_wq;
 static unsigned int boost_ms = 50;
 module_param(boost_ms, uint, 0644);
 
+static unsigned int sync_threshold;
+module_param(sync_threshold, uint, 0644);
 /*
  * The CPUFREQ_ADJUST notifier is used to override the current policy min to
  * make sure policy min >= boost_min. The cpufreq framework then does the job
@@ -131,8 +133,18 @@ static int boost_mig_sync_thread(void *data)
 			continue;
 		}
 
+		if (sync_threshold && (dest_policy.cur >= sync_threshold))
+			continue;
+
 		cancel_delayed_work_sync(&s->boost_rem);
-		s->boost_min = src_policy.cur;
+		if (sync_threshold) {
+			if (src_policy.cur >= sync_threshold)
+				s->boost_min = sync_threshold;
+			else
+				s->boost_min = src_policy.cur;
+		} else {
+			s->boost_min = src_policy.cur;
+		}
 		/* Force policy re-evaluation to trigger adjust notifier. */
 		get_online_cpus();
 		if (cpu_online(dest_cpu)) {
