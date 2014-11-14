e7f1eaf1849aaa5353f3e247d2de194d1f628a43
MassStash <MassStash@gmail.com>
2014-11-13 22:30:36 -0600

    Initial source push

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Nov 4 12:06:43 CST 2014"
+#define LINUX_COMPILE_BY "Aaron"
+#define LINUX_COMPILE_HOST "kernel1-build2"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-g72fda8f0-dirty"
